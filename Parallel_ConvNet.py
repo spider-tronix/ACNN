07e7197152cd99287eabab93df26479cf5227f8e
syzygianinfern0 <spsharan2000@gmail.com>
2019-09-16 23:12:15 +0530

    Move to GPU

--- Parallel_ConvNet.py
+++ Parallel_ConvNet.py
@@ -1,9 +1,4 @@
-from numpy.random import seed
-seed(0)
 import torch
-torch.manual_seed(0)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
 import torch.nn as nn
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
@@ -14,18 +9,26 @@ from bokeh.models import LinearAxis, Range1d
 from torchviz import make_dot
 import numpy as np
 
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+np.random.seed(0)
+torch.manual_seed(0)
+
 num_epochs = 3
 num_classes = 10
 batch_size = 100
 learning_rate = 0.01
 
-#loading_data
-data_loc = '/home/ruphan/WorkSpace/ACNN/MNISTData'
+# Loading Data
+data_loc = '/media/syzygianinfern0/Summore Data/mnist_data'
 t = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
 train_dataset = torchvision.datasets.MNIST(root=data_loc, train=True, transform=t, download=True)
 test_dataset = torchvision.datasets.MNIST(root=data_loc, train=False, transform=t)
 train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
 
 class ParallelNets(nn.Module):
     def __init__(self):
@@ -44,21 +47,23 @@ class ParallelNets(nn.Module):
         xout = self.net1conv2(xout)
         xout = xout.reshape(xout.size(0), -1)
         xout = self.net1fc1(xout)
-        #xout = xout.detach()
+        # xout = xout.detach()
         yout = self.net2conv1(y)
         yout = self.net2conv2(yout)
         yout = self.net2conv3(yout)
         yout = yout.reshape(yout.size(0), -1)
         yout = self.net2fc1(yout)
-        #yout = yout.detach()
+        # yout = yout.detach()
         out = torch.cat((xout, yout), dim=1)
         out = self.dense(out)
         return out
 
+
 model = ParallelNets()
-x = torch.zeros(1, 1, 28, 28, dtype=torch.float, requires_grad=False)
-out = model(x,x)
-make_dot(out).render("GradientFlowGraph", format="png")
+model.to(device)
+# x = torch.zeros(1, 1, 28, 28, dtype=torch.float, requires_grad=False)
+# out = model(x, x)
+# make_dot(out).render("GradientFlowGraph", format="png")
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
@@ -66,7 +71,8 @@ steps = len(train_loader)
 loss_list = []
 acc_list = []
 for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):
+    for i, data in enumerate(train_loader):
+        images, labels = data[0].to(device), data[1].to(device)
         output = model(images, images)
         loss = criterion(output, labels)
         loss_list.append(loss.item())
@@ -78,21 +84,24 @@ for epoch in range(num_epochs):
         correct = (predicted == labels).sum().item()
         acc_list.append(correct / total)
         if (i + 1) % 100 == 0:
-            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {}%' .format(epoch + 1, num_epochs, i + 1, steps, loss.item(), (correct / total) * 100))
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {}%'.format(epoch + 1, num_epochs, i + 1, steps,
+                                                                                    loss.item(),
+                                                                                    (correct / total) * 100))
 
 # Testing
 model.eval()
 with torch.no_grad():
     correct = 0
     total = 0
-    for images, labels in test_loader:
+    for data in test_loader:
+        images, labels = data[0].to(device), data[1].to(device)
         output = model(images, images)
         _, predicted = torch.max(output.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
     print('Test Accuracy of the model on the 10000 test images: {} %'.format((correct / total) * 100))
 
-#Plotting
+# Plotting
 p = figure(y_axis_label='Loss', width=600, y_range=(0, 1), title='Results')
 p.extra_y_ranges = {'Accuracy': Range1d(start=0, end=100)}
 p.add_layout(LinearAxis(y_range_name='Accuracy', axis_label='Accuracy (%)'), 'right')
