e05e08bebb16f930db4983ecb68b02c0ba1acff2
syzygianinfern0 <spsharan2000@gmail.com>
2019-09-19 04:24:01 +0530

    Implement Train Loop

--- agents.py
+++ agents.py
@@ -1,18 +1,19 @@
 import torch
 import torch.nn as nn
+from torchviz import make_dot
 
 
-class ParallelNets(nn.Module):
+class Viba(nn.Module):
     """Branches of the Network"""
 
-    def __init__(self, net1_channels=(1, 10, 20),
-                 net2_channels=(1, 10, 20, 30),
+    def __init__(self, net1_channels=[1, 10, 20],
+                 net2_channels=[1, 10, 20, 30],
                  kernel_size=5, stride=1, padding=2,
                  junction_channels=30):
         """
         Init all variables for class
         """
-        super(ParallelNets, self).__init__()
+        super(Viba, self).__init__()
         self.net1conv1 = nn.Sequential(
             nn.Conv2d(net1_channels[0], net1_channels[1],
                       kernel_size=kernel_size, stride=stride, padding=padding),
@@ -22,6 +23,7 @@ class ParallelNets(nn.Module):
             nn.Conv2d(net1_channels[1], net1_channels[2],
                       kernel_size=kernel_size, stride=stride, padding=padding),
             nn.ReLU()
+
         )
         self.net2conv1 = nn.Sequential(
             nn.Conv2d(net2_channels[0], net2_channels[1],
@@ -38,7 +40,7 @@ class ParallelNets(nn.Module):
                       kernel_size=kernel_size, stride=stride, padding=padding),
             nn.ReLU()
         )
-        self.junction = nn.Conv2d(in_channels=net1_channels[-1],
+        self.junction = nn.Conv3d(in_channels=net1_channels[-1],
                                   out_channels=junction_channels,
                                   kernel_size=28,
                                   stride=stride,
@@ -57,7 +59,9 @@ class ParallelNets(nn.Module):
         out2 = self.net2conv3(out2)
 
         with torch.no_grad():
-            self.junction.weight = nn.Parameter(out2)
+            # noinspection PyArgumentList
+            # self.junction.weight = nn.Parameter(torch.transpose(torch.repeat_interleave(out2, 20, 0),1, 0))
+            self.junction.weight = nn.Parameter(out2[:, :, None, :, :].repeat(1, 1, 20, 1, 1))
 
         out_junction = self.junction(out1)
 
---
+++ main.py
@@ -0,0 +1,69 @@
+from agents import Viba
+import torch
+from torch import nn
+import numpy as np
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import torchvision.datasets
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # Prefer GPU
+
+np.random.seed(0)
+torch.manual_seed(0)
+
+# HyperParams and Others
+num_epochs = 3
+num_classes = 10
+batch_size = 100
+learning_rate = 0.01
+
+model = Viba()
+
+if __name__ == '__main__':
+    # Loading Data
+    data_loc = '/media/syzygianinfern0/Summore Data/Datasets'
+    t = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+    train_dataset = torchvision.datasets.MNIST(root=data_loc, train=True, transform=t, download=True)
+    test_dataset = torchvision.datasets.MNIST(root=data_loc, train=False, transform=t)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+    model = Viba()
+    model.to(device=device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    steps = len(train_loader)
+    loss_list = []
+    acc_list = []
+    for epoch in range(num_epochs):
+        for i, data in enumerate(train_loader):
+            images, labels = data[0].to(device), data[1].to(device)
+            output = model(images, images)
+            loss = criterion(output, labels)
+            loss_list.append(loss.item())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            total = labels.size(0)
+            _, predicted = torch.max(output.data, 1)  # Get prediction
+            correct = (predicted == labels).sum().item()  # Evaluate
+            acc_list.append(correct / total)
+            if (i + 1) % 100 == 0:
+                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {}%'.format(epoch + 1, num_epochs, i + 1,
+                                                                                        steps,
+                                                                                        loss.item(),
+                                                                                        (correct / total) * 100))
+    # Testing
+    model.eval()
+    with torch.no_grad():  # Dont track variables during evaluation
+        correct = 0
+        total = 0
+        for data in test_loader:
+            images, labels = data[0].to(device), data[1].to(device)
+            output = model(images, images)
+            _, predicted = torch.max(output.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+        print('Test Accuracy of the model on the 10000 test images: {} %'.format((correct / total) * 100))
