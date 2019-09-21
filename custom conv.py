255a865912821c02f71615a6e4b03e13da79c534
syzygianinfern0 <spsharan2000@gmail.com>
2019-09-21 16:07:59 +0530

    Fix grad not tracked

--- custom conv.py
+++ custom conv.py
@@ -1,14 +1,22 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 
-wts = torch.arange(24).reshape(3, 2, 2, 2).to(dtype=torch.float)
-inps = torch.arange(18).reshape(1, 2, 3, 3).to(dtype=torch.float)
+in1 = torch.arange(8).reshape(1, 2, 2, 2).to(dtype=torch.float)
+# in2 = torch.arange(9).reshape(1, 1, 3, 3).to(dtype=torch.float)
 
-conv = nn.Conv2d(2, 3, 2, bias=False)
+conv1 = nn.Conv2d(2, 5, 2, bias=False)
+conv2 = nn.Conv2d(2, 5, 2, bias=False)
 
-with torch.no_grad():
-    conv.weight = nn.Parameter(wts)
 
-output = conv(inps)
-output.mean().backward()
-print(conv.weight.grad)
+output1 = conv1(in1)
+output2 = conv2(in1)
+
+
+# conv = nn.Conv2d(5, 5, 1, bias=False)
+# conv.weight = nn.Parameter(output1)
+# output3 = conv(output2)
+
+output3 = F.conv2d(output1, output2)
+
+output3.backward()
