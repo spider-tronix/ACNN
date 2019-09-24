b91bdab3b15e93c603e9108a7507180c5dc3cd83
Sachin Kumar <sachinkumar04428@gmail.com>
2019-09-24 17:25:40 +0530

    benchmark model added

---
+++ Benchmark/base_model.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class BaseModel(nn.Module):
+    def __init__(self):
+        super(BaseModel, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, (3,3))
+        self.conv2 = nn.Conv2d(16, 64, (3,3))
+        self.conv3 = nn.Conv2d(64, 128, (3,3))
+        self.fc1 = nn.Linear(128*22*22, 1024)
+        self.fc2 = nn.Linear(1024, 128)
+        self.fc3 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        batch_size = x.shape[0]
+        x = x.view(batch_size, -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.softmax(self.fc3(x))
+        return x
