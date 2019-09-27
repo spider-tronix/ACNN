b53473d9f7d055afe773d4389a1f5e88febdd1a5
Syzygianinfern0 <spsharan2000@gmail.com>
2019-09-27 21:50:31 +0530

    Fix missing docstring

--- Resnet-10/resnet_agent.py
+++ Resnet-10/resnet_agent.py
@@ -15,11 +15,21 @@ def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
 
 
 class BasicBlock(nn.Module):
-    def __init__(self, inplanes, planes, stride=1):
+    """Handles a single Resnet Block"""
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        """
+        Init all variables for class
+        :param inplanes: Input channels
+        :param planes: Out put Channels
+        :param stride: Stride for Conv layer
+        :param downsample: Flag to indicate in channel dimension
+        """
         super(BasicBlock, self).__init__()
         norm_layer = nn.BatchNorm2d
+        self.downsample = downsample
 
-        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = norm_layer(planes)
         self.relu = nn.ReLU(inplace=True)
@@ -28,7 +38,12 @@ class BasicBlock(nn.Module):
         self.stride = stride
 
     def forward(self, x):
-        identity = x
+        """
+        Forward Prop Implementation
+        :param x: Input to single block. Either from prior block or initial input to network
+        :return: Output from single Resnet block. To pass into next block or as output of last block
+        """
+        identity = x  # Remember input
 
         out = self.conv1(x)
         out = self.bn1(out)
@@ -37,20 +52,30 @@ class BasicBlock(nn.Module):
         out = self.conv2(out)
         out = self.bn2(out)
 
-        out += identity
+        if self.downsample is not None:
+            identity = self.downsample(x)  # Down Sampling to adjust channel dimension
+
+        out += identity  # Magic!!
         out = self.relu(out)
 
         return out
 
 
 class ResNet(nn.Module):
-    def __init__(self, architecture, num_classes):
+    """Handles entire structure"""
+
+    def __init__(self, architecture=[1, 1, 1, 1], num_classes=10):
+        """
+
+        :param architecture: Decided from required depth of network
+        :param num_classes: Possible number of unique labels of classification
+        """
         super(ResNet, self).__init__()
         norm_layer = nn.BatchNorm2d
         self._norm_layer = norm_layer
 
-        self.inplanes = 64  # <<<<<<<
-        self.dilation = 1  # <<<<<<<
+        self.inplanes = 64
+        self.dilation = 1
 
         self.conv1 = nn.Conv2d(1, 64,
                                kernel_size=7, stride=2, padding=3,
@@ -58,10 +83,10 @@ class ResNet(nn.Module):
         self.bn1 = norm_layer(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(64, architecture[0])
-        self.layer2 = self._make_layer(128, architecture[1], stride=2)
-        self.layer3 = self._make_layer(256, architecture[2], stride=2)
-        self.layer4 = self._make_layer(512, architecture[3], stride=2)
+        self.layer1 = self._make_layer(64, architecture[0])  # Layer 1
+        self.layer2 = self._make_layer(128, architecture[1], stride=2)  # Layer 2
+        self.layer3 = self._make_layer(256, architecture[2], stride=2)  # Layer 3
+        self.layer4 = self._make_layer(512, architecture[3], stride=2)  # Layer 4
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512 * BasicBlock.expansion, num_classes)
         self._zero_init()
@@ -81,18 +106,25 @@ class ResNet(nn.Module):
                 nn.init.constant_(m.bias, 0)
 
     def _make_layer(self, planes, blocks, stride=1):
+        """
+        Concatenates BasicBlocks to create a single unit
+        :param planes: Output channels of BasicBlocks
+        :param blocks: Number of BasicBlocks to concatenate
+        :param stride: Stride for Conv operation
+        :return: Single unit of ResNet as a Sequential model
+        """
         block = BasicBlock
         norm_layer = self._norm_layer
         downsample = None
 
         if stride != 1 or self.inplanes != planes * block.expansion:
+            """Generate down sampling based on existence of mismatch of dimensions"""
             downsample = nn.Sequential(
                 conv1x1(self.inplanes, planes * block.expansion, stride),
                 norm_layer(planes * block.expansion),
             )
 
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
+        layers = [block(self.inplanes, planes, stride, downsample)]
         self.inplanes = planes * block.expansion
         for _ in range(1, blocks):
             layers.append(block(self.inplanes, planes))
@@ -100,6 +132,11 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
+        """
+        Forward Prop of Final Network
+        :param x: Input image from an iterating dataloader with batch dimension
+        :return: Output after propagation
+        """
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
@@ -113,13 +150,12 @@ class ResNet(nn.Module):
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
-        return F.log_softmax(x)
+        return F.log_softmax(x, dim=1)  # Thanks to https://github.com/sachin-101 for a great tip
 
 
-def resnet(architecture=[1, 1, 1, 1],
-           num_classes=10):
+def resnet(architecture=[1, 1, 1, 1], num_classes=10):
     r"""ResNet-18 model from
     `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
     """
-    model = ResNet(architecture, num_classes)
+    model = ResNet(architecture, num_classes=num_classes)
     return model
