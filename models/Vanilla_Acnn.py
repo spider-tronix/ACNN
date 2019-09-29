ace592eaed2210bdc4069f1786ce73d7533d2f5e
Sachin Kumar <sachinkumar04428@gmail.com>
2019-09-29 23:59:30 +0530

    Removed Folders for Models
    Folders had single files. Seemed very cluttered.

---
+++ models/Vanilla_Acnn.py
@@ -0,0 +1,59 @@
+import torch.nn as nn
+# noinspection PyPep8Naming
+from utilities.train_helpers import grouped_conv
+
+
+class ACNN(nn.Module):
+    """Branches of the Network"""
+
+    def __init__(self,
+                 net1_channels=(1, 16, 32),
+                 net2_channels=(1, 16, 32, 64)):
+        super(ACNN, self).__init__()
+
+        self.net1 = nn.Sequential(
+            nn.Conv2d(net1_channels[0], net1_channels[1],
+                      kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.Conv2d(net1_channels[1], net1_channels[2],
+                      kernel_size=5, stride=2),
+            nn.ReLU()
+        )
+
+        self.net2 = nn.Sequential(
+            nn.Conv2d(net2_channels[0], net2_channels[1],
+                      kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.Conv2d(net2_channels[1], net2_channels[2],
+                      kernel_size=5, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(net2_channels[2], net2_channels[3],
+                      kernel_size=5, stride=2),
+            nn.ReLU()
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(4096, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 256),
+            nn.ReLU(),
+            nn.Linear(256, 64),
+            nn.ReLU(),
+            nn.Linear(64, 10),
+            nn.LogSoftmax(dim=1)
+        )
+
+    # noinspection PyPep8Naming
+    def forward(self, X):
+        """
+        Forward Prop
+        :param X: Input dataset with batch dimension
+        :return: Output of model and parameters
+        """
+        out1 = self.net1(X)
+        out2 = self.net2(X)
+
+        out3 = grouped_conv(out1, out2)
+
+        out3 = self.fc(out3)
+        return out3
