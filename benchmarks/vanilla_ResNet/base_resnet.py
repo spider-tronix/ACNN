a916f64feeef9b1724b1b2f0f52693c3ad81e839
Sachin Kumar <sachinkumar04428@gmail.com>
2019-09-29 23:53:56 +0530

    Benchmark Resnet changed
    Moved the older benchmarks to archive. Added latest benchmark for better comparison.

---
+++ benchmarks/vanilla_ResNet/base_resnet.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+# noinspection PyPep8Naming
+import torch.nn.functional as F
+
+
+class BaseResNet(nn.Module):
+
+    def __init__(self, device='cuda:0'):
+        """
+        Init all variables for class
+        """
+        super(BaseResNet, self).__init__()
+        self.device = device
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, 3, stride=1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 16, 3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(16, 16, 3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(16)
+        )
+
+        self.downsample_2_3 = nn.Sequential(
+            nn.Conv2d(16, 32, 1, stride=2, bias=False),
+            nn.BatchNorm2d(32)
+        )
+
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(16, 32, 3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 32, 3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(32)
+        )
+
+        self.downsample_3_4 = nn.Sequential(
+            nn.Conv2d(32, 64, 1, stride=2, bias=False),
+            nn.BatchNorm2d(64)
+        )
+
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(32, 64, 3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, 3, stride=1, padding=1, bias=False)
+        )
+
+        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+
+        self.fc = nn.Sequential(
+            nn.Linear(64, 10),
+            nn.LogSoftmax(dim=1)
+        )
+
+    # noinspection PyPep8Naming
+    def forward(self, X):
+        """
+        forward propagation logic
+        """
+
+        out1 = self.layer1(X)
+
+        identity1 = out1
+        out2 = self.layer2(out1)
+        out2 += identity1  # adding residual
+        F.relu(out2)
+
+        identity2 = self.downsample_2_3(out2)
+        out3 = self.layer3(out2)
+        out3 += identity2
+        F.relu(out3)
+
+        identity3 = self.downsample_3_4(out3)
+        out4 = self.layer4(out3)
+        out4 += identity3
+        F.relu(out4)
+
+        out = self.avgpool(out4)
+        out = torch.flatten(out, 1)
+        out = self.fc(out)
+
+        return out
