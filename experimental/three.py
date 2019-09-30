6a7e2deb7484e3919fa19d3265f9a6760b99d143
Sachin Kumar <sachinkumar04428@gmail.com>
2019-09-30 21:06:48 +0530

    logging enabled in test

--- experimental/three.py
+++ experimental/three.py
@@ -20,12 +20,13 @@ def train(model: nn.Module, device,
     :param epoch: Epoch number for logging purpose
     :param log_interval: Print stats for every
     :param writer: Tensorboard writer to track training accuracy
+    :param logger: Tuple of numpy array to log values 
     :return: new logger
     """
     if logger is not None:
-        step, train_loss, train_accuracy = logger
+        step_log, loss_log, acc_log = logger
     else:
-        step, train_loss, train_accuracy = None, None, None
+        step_log, loss_log, acc_log = None, None, None
 
     model.train()
     running_loss = 0.0
@@ -39,7 +40,7 @@ def train(model: nn.Module, device,
 
         optimizer.zero_grad()
         loss.backward()
-        optimizer.step()  # Backprop
+        optimizer.step_log()  # Backprop
         # Logging
         pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
         correct += pred.eq(target.view_as(pred)).sum().item()
@@ -60,13 +61,59 @@ def train(model: nn.Module, device,
                 writer.add_scalar('Training Accuracy', a, global_step)
 
             if logger is not None:
-                step = np.append(step, global_step)
-                train_accuracy = np.append(train_accuracy, a)
-                train_loss = np.append(train_loss, l)
+                step_log = np.append(step_log, global_step)
+                acc_log = np.append(acc_log, a)
+                loss_log = np.append(loss_log, l)
 
             running_loss = 0.0
 
     print('\nTraining Accuracy: {}/{} ({:.4f}%)'.format(correct, len(train_loader.dataset),
                                                         100. * correct / len(train_loader.dataset)))
 
-    return step, train_loss, train_accuracy
+    return step_log, loss_log, acc_log
+
+def test(model: nn.Module, device, test_loader: torch.utils.data.DataLoader, 
+        epoch, writer=None, logger=None):
+    """
+    Performs evaluation on dataset
+    :param model: Model Class
+    :param device: Device to test on. Use 'cuda:0' for GPU acceleration
+    :param test_loader: Iterable Dataset
+    :param epoch: epoch number
+    :param writer: Tensorboard writer to track test accuracy
+    :param logger: Tuple of numpy array to log values
+    :return:
+    """
+
+    if logger is not None:
+        step_log, loss_log, acc_log = logger
+    else:
+        step_log, loss_log, acc_log = None, None, None
+
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    test_acc = 100. * correct / len(test_loader.dataset)
+
+    if writer is not None:  # Tensorboard Logging
+        writer.add_scalar('Test Loss (/epoch)', test_loss, epoch)
+        writer.add_scalar('Test Accuracy (/epoch)', test_acc, epoch)
+    
+    if logger is not None: # Manual Logging
+            step_log = np.append(step_log, epoch)
+            acc_log = np.append(acc_log, test_acc)
+            loss_log = np.append(loss_log, test_loss)
+
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset), test_acc))
+
+    return step_log, loss_log, acc_log
