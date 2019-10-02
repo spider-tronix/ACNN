af2609f3b115adeb0dd3d412be3f6d41148dc987
Syzygianinfern0 <spsharan2000@gmail.com>
2019-10-02 05:34:01 +0530

    Utility function for data normalisation Can be remade to be called by load_data during preprocessing

--- utilities/data.py
+++ utilities/data.py
@@ -1,3 +1,4 @@
+import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torchvision import datasets
@@ -33,3 +34,34 @@ def load_data(data_loc, batch_size, download=False, dataset='MNIST'):
     test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
     return train_loader, test_loader
+
+
+def _get_mean_std(dataset='MNIST',
+                  data_loc=r'E:\Datasets'):
+    """
+    Provides insights on the shape, mean and std of dataset
+    The mean and std are to be used in the actual preprocessing of the dataset
+    https://discuss.pytorch.org/t/normalization-in-the-mnist-example/457/8
+    :param dataset: Name of the dataset
+    :param data_loc: Absolute location of data
+    :return: None
+    """
+    if dataset not in ['MNIST', 'SVHN']:
+        raise NotImplementedError('Only MNIST/SVHN')
+
+    train_transform = transforms.Compose([transforms.ToTensor()])
+
+    if dataset is 'MNIST':
+        data_set = torchvision.datasets.MNIST(
+            root=data_loc, train=True, download=True, transform=train_transform)
+        print(list(data_set.train_data.size()))
+        print(data_set.train_data.float().mean() / 255)
+        print(data_set.train_data.float().std() / 255)
+
+    elif dataset is 'SVHN':
+        data_set = torchvision.datasets.SVHN(
+            root=data_loc, split='train', download=False,
+            transform=train_transform)
+        print(f"[{data_set.data.size}, {list(next(iter(data_set))[0].shape)}]")
+        print(data_set.data.mean() / 255)
+        print(data_set.data.std() / 255)
