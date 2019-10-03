40a71de1eb4ada83c463cca18477189b6e903d8c
Syzygianinfern0 <spsharan2000@gmail.com>
2019-10-03 19:02:56 +0530

    Another self.n -_- Indentation Errors

--- experimental/cifar_resnet.py
+++ experimental/cifar_resnet.py
@@ -4,10 +4,10 @@ import torch.nn.functional as F
 
 
 class CifarResNet(nn.Module):
-"""
-    All in one ResNet model for training on CIFAR dataset.
-    Implements ResNet-20, ResNet-32, ResNet-44, ResNet-56, ResNet-110.
-"""
+    """
+        All in one ResNet model for training on CIFAR dataset.
+        Implements ResNet-20, ResNet-32, ResNet-44, ResNet-56, ResNet-110.
+    """
 
     def __init__(self, n):
         """
@@ -17,17 +17,17 @@ class CifarResNet(nn.Module):
 
         if n not in [3, 5, 7, 9, 11]:
             raise NotImplementedError('Resnet 20/32/44/56/110 only implemented. \
-                                        Contact Sharan or Sachin, if u need any other model')
+                                            Contact Sharan or Sachin, if u need any other model')
         self.n = n
 
         l = 1  # keeps count of layers added
         self.model = nn.Sequential()
-        self.model.append(f'conv{l}', nn.Conv2d(1, 16, 3, stride=1, padding=1, bias=False)) 
+        self.model.append(f'conv{l}', nn.Conv2d(1, 16, 3, stride=1, padding=1, bias=False))
         self.model.append(f'bn{l}', nn.BatchNorm2d(16))
         self.model.append(f'relu{l}', nn.ReLU())
         l += 1
-        
-        for _ in range(2*n):
+
+        for _ in range(2 * n):
             self.model.append(f'conv{l}', nn.Conv2d(16, 16, 3, stride=1, padding=1, bias=False))
             self.model.append(f'bn{l}', nn.BatchNorm2d(16))
             self.model.append(f'relu{l}', nn.ReLU())
@@ -40,7 +40,7 @@ class CifarResNet(nn.Module):
         self.modle.append(f'bn{l}', nn.BatchNorm2d(32))
         l += 1
 
-        for _ in range(2*n - 1):
+        for _ in range(2 * n - 1):
             self.model.append(f'conv{l}', nn.Conv2d(16, 32, 3, stride=2, padding=1, bias=False))
             self.model.append(f'bn{l}', nn.BatchNorm2d(32))
             self.model.append(f'relu{l}', nn.ReLU())
@@ -53,7 +53,7 @@ class CifarResNet(nn.Module):
         self.modle.append(f'bn{l}', nn.BatchNorm2d(64))
         l += 1
 
-        for _ in range(2*n - 1):
+        for _ in range(2 * n - 1):
             self.model.append(f'conv{l}', nn.Conv2d(32, 64, 3, stride=2, padding=1, bias=False))
             self.model.append(f'bn{l}', nn.BatchNorm2d(64))
             self.model.append(f'relu{l}', nn.ReLU())
@@ -75,22 +75,22 @@ class CifarResNet(nn.Module):
         forward propagation logic
         """
 
-        layers = { name:module for name, module in self.model.named_modules()}
-        id_dict = { f'identity{i}':None for i in range(1, 6*self.n+1)}
+        layers = {name: module for name, module in self.model.named_modules()}
+        id_dict = {f'identity{i}': None for i in range(1, 6 * self.n + 1)}
 
         out = layers['conv1'](X)
         out = layers['bn1'](out)
         out = layers['relu1'](out)
 
-        for i in range(2, 6*self.n+2):
-            id_dict[f'identity{i-1}'] = out
+        for i in range(2, 6 * self.n + 2):
+            id_dict[f'identity{i - 1}'] = out
             out = layers[f'conv{i}'](out)
             out = layers[f'bn{i}'](out)
             out = layers[f'relu{i}'](out)
-            if i < 6*n + 1:
-                out = layers[f'conv{i+1}'](out)
-                out = layers[f'bn{i+1}'](out)
-                out += id_dict[f'identity{i-1}']
+            if i < 6 * self.n + 1:
+                out = layers[f'conv{i + 1}'](out)
+                out = layers[f'bn{i + 1}'](out)
+                out += id_dict[f'identity{i - 1}']
                 out = F.relu(out)
 
         out = self.avgpool(out)
