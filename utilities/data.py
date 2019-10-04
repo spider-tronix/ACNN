90afc14451f7eed49939b6b8f89c3af8e8e1326b
Syzygianinfern0 <spsharan2000@gmail.com>
2019-10-04 23:46:19 +0530

    Load data for CIFAR10

--- utilities/data.py
+++ utilities/data.py
@@ -1,10 +1,11 @@
+import numpy as np
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torchvision import datasets
 
 
-def load_data(data_loc, batch_size, download=False, dataset='MNIST'):
+def load_data(data_loc, batch_size, download=True, dataset='MNIST'):
     """
     Downloads data or uses existing. Packs into iterable Dataloader
     :param dataset: Name of Dataset (MNIST | SVHN)
@@ -16,52 +17,127 @@ def load_data(data_loc, batch_size, download=False, dataset='MNIST'):
     train_dataset = None
     test_dataset = None
 
-    if dataset not in ['MNIST', 'SVHN', 'CIFAR10']:
-        raise NotImplementedError('Only MNIST/SVHN/CIFAR10')
+    if dataset not in ['MNIST', 'CIFAR10']:
+        raise NotImplementedError('Only MNIST/CIFAR10')
 
-    t = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
     if dataset == 'MNIST':
+        t = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((mean_mnist(data_loc=data_loc).item(),),
+                                                     (std_mnist(data_loc=data_loc).item(),))])
         train_dataset = datasets.MNIST(root=data_loc, train=True, transform=t, download=download)
         test_dataset = datasets.MNIST(root=data_loc, train=False, transform=t, download=download)
-    elif dataset == 'SVHN':
-        train_dataset = datasets.SVHN(root=data_loc, split='train', transform=t, download=download)
-        test_dataset = datasets.SVHN(root=data_loc, split='test', transform=t, download=download)
-    elif dataset == 'CIFAR10':
-        train_dataset = datasets.CIFAR10(root=data_loc, train=True, transform=t, download=download)
-        test_dataset = datasets.CIFAR10(root=data_loc, train=False, transform=t, download=download)
 
-    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+    elif dataset == 'CIFAR10':
+        transform_train = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean_cifar10(data_loc), std_cifar10(data_loc)),
+        ])
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean_cifar10(data_loc), std_cifar10(data_loc)),
+        ])
+        train_dataset = datasets.CIFAR10(root=data_loc, train=True, transform=transform_train, download=download)
+        test_dataset = datasets.CIFAR10(root=data_loc, train=False, transform=transform_test, download=download)
+
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
 
     return train_loader, test_loader
 
 
-def _get_mean_std(dataset='MNIST',
-                  data_loc=r'E:\Datasets'):
+def mean_mnist(data_loc='./data'):
     """
     Provides insights on the shape, mean and std of dataset
     The mean and std are to be used in the actual preprocessing of the dataset
     https://discuss.pytorch.org/t/normalization-in-the-mnist-example/457/8
-    :param dataset: Name of the dataset
     :param data_loc: Absolute location of data
-    :return: None
+    :return: Mean eg: [0.49139968 0.48215841 0.44653091]
     """
-    if dataset not in ['MNIST', 'SVHN']:
-        raise NotImplementedError('Only MNIST/SVHN')
-
     train_transform = transforms.Compose([transforms.ToTensor()])
+    data_set = torchvision.datasets.MNIST(
+        root=data_loc, train=True, download=True,
+        transform=train_transform)
 
-    if dataset is 'MNIST':
-        data_set = torchvision.datasets.MNIST(
-            root=data_loc, train=True, download=True, transform=train_transform)
-        print(list(data_set.train_data.size()))
-        print(data_set.train_data.float().mean() / 255)
-        print(data_set.train_data.float().std() / 255)
+    return data_set.train_data.float().mean() / 255
 
-    elif dataset is 'SVHN':
-        data_set = torchvision.datasets.SVHN(
-            root=data_loc, split='train', download=False,
-            transform=train_transform)
-        print(f"[{data_set.data.size}, {list(next(iter(data_set))[0].shape)}]")
-        print(data_set.data.mean() / 255)
-        print(data_set.data.std() / 255)
+
+def std_mnist(data_loc='./data'):
+    """
+    Provides insights on the shape, mean and std of dataset
+    The mean and std are to be used in the actual preprocessing of the dataset
+    https://discuss.pytorch.org/t/normalization-in-the-mnist-example/457/8
+    :param data_loc: Absolute location of data
+    :return: Std eg: [0.24703223 0.24348513 0.26158784]
+    """
+    train_transform = transforms.Compose([transforms.ToTensor()])
+    data_set = torchvision.datasets.MNIST(
+        root=data_loc, train=True, download=True,
+        transform=train_transform)
+
+    return data_set.train_data.float().std() / 255
+
+
+def mean_cifar10(data_loc='./data'):
+    """
+    Provides insights on the shape, mean and std of dataset
+    The mean and std are to be used in the actual preprocessing of the dataset
+    https://discuss.pytorch.org/t/normalization-in-the-mnist-example/457/8
+    :param data_loc: Absolute location of data
+    :return: Mean eg: [0.49139968 0.48215841 0.44653091]
+    """
+    train_transform = transforms.Compose([transforms.ToTensor()])
+    data_set = torchvision.datasets.CIFAR10(
+        root=data_loc, train=True, download=True,
+        transform=train_transform)
+
+    return list(data_set.data.mean(axis=(0, 1, 2)) / 255)
+
+
+def std_cifar10(data_loc='./data'):
+    """
+    Provides insights on the shape, mean and std of dataset
+    The mean and std are to be used in the actual preprocessing of the dataset
+    https://discuss.pytorch.org/t/normalization-in-the-mnist-example/457/8
+    :param data_loc: Absolute location of data
+    :return: Std eg: [0.24703223 0.24348513 0.26158784]
+    """
+    train_transform = transforms.Compose([transforms.ToTensor()])
+    data_set = torchvision.datasets.CIFAR10(
+        root=data_loc, train=True, download=True,
+        transform=train_transform)
+
+    return list(data_set.data.std(axis=(0, 1, 2)) / 255)
+
+
+def mean_cifar100(data_loc='./data'):
+    """
+    Provides insights on the shape, mean and std of dataset
+    The mean and std are to be used in the actual preprocessing of the dataset
+    https://discuss.pytorch.org/t/normalization-in-the-mnist-example/457/8
+    :param data_loc: Absolute location of data
+    :return: Mean eg: [0.50707516 0.48654887 0.44091784]
+    """
+    train_transform = transforms.Compose([transforms.ToTensor()])
+    data_set = torchvision.datasets.CIFAR100(
+        root=data_loc, train=True, download=True,
+        transform=train_transform)
+
+    return list(np.mean(data_set.data, axis=(0, 1, 2)) / 255)
+
+
+def std_cifar100(data_loc='./data'):
+    """
+    Provides insights on the shape, mean and std of dataset
+    The mean and std are to be used in the actual preprocessing of the dataset
+    https://discuss.pytorch.org/t/normalization-in-the-mnist-example/457/8
+    :param data_loc: Absolute location of data
+    :return: Std eg: [0.26733429 0.25643846 0.27615047]
+    """
+    train_transform = transforms.Compose([transforms.ToTensor()])
+    data_set = torchvision.datasets.CIFAR100(
+        root=data_loc, train=True, download=True,
+        transform=train_transform)
+
+    return list(np.std(data_set.data, axis=(0, 1, 2)) / 255)
