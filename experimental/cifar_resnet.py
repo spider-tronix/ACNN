682d1baff9c89e4ae1a52cf7ecc8f9e4469429de
Sachin Kumar <sachinkumar04428@gmail.com>
2019-10-04 12:24:43 +0530

    subsampling added

--- experimental/cifar_resnet.py
+++ experimental/cifar_resnet.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
+
+            
 class CifarResNet(nn.Module):
     """
         All in one ResNet model for training on CIFAR dataset.
@@ -20,55 +22,20 @@ class CifarResNet(nn.Module):
                                             Contact Sharan or Sachin, if u need any other model')
         self.n = n
 
-        l = 1  # keeps count of layers added
+        self.l = 1  # keeps count of layers added
         self.relu_list = np.zeros(6*n+2)
 
         self.model = nn.Sequential()
-        self.model.add_module(f'conv{l}', nn.Conv2d(3, 16, 3, stride=1, padding=1, bias=False))
-        self.model.add_module(f'bn{l}', nn.BatchNorm2d(16))
-        self.model.add_module(f'relu{l}', nn.ReLU())
-        self.relu_list[l] = 1
-        l += 1
-        
+        self.model.add_module(f'conv{self.l}', nn.Conv2d(3, 16, 3, stride=1, padding=1, bias=False))
+        self.model.add_module(f'bn{self.l}', nn.BatchNorm2d(16))
+        self.model.add_module(f'relu{self.l}', nn.ReLU())
+        self.relu_list[self.l] = 1
+        self.l += 1
 
-        for _ in range(n):
-            self.model.add_module(f'conv{l}', nn.Conv2d(16, 16, 3, stride=1, padding=1, bias=False))
-            self.model.add_module(f'bn{l}', nn.BatchNorm2d(16))
-            self.model.add_module(f'relu{l}', nn.ReLU())
-            self.relu_list[l] = 1
-            l += 1
-            self.model.add_module(f'conv{l}', nn.Conv2d(16, 16, 3, stride=1, padding=1, bias=False))
-            self.model.add_module(f'bn{l}', nn.BatchNorm2d(16))
-            l += 1
-            
+        self.grouped_resnet_blocks(in_c=16, out_c=16)
+        self.grouped_resnet_blocks(in_c=16, out_c=32, sub=1)
+        self.grouped_resnet_blocks(in_c=32, out_c=64, sub=2)
         
-        for i in range(n):
-            if i == 0:
-                self.model.add_module(f'conv{l}', nn.Conv2d(16, 32, 1, stride=2, bias=False)) # k_size = 1, stride = 2, pad = 0 for downsampling
-            else:
-                self.model.add_module(f'conv{l}', nn.Conv2d(32, 32, 3, stride=1, padding=1, bias=False)) # k_size = 3, stride = 1
-            self.model.add_module(f'bn{l}', nn.BatchNorm2d(32))
-            self.model.add_module(f'relu{l}', nn.ReLU())
-            self.relu_list[l] = 1
-            l += 1
-            self.model.add_module(f'conv{l}', nn.Conv2d(32, 32, 3, stride=1, padding=1, bias=False))
-            self.model.add_module(f'bn{l}', nn.BatchNorm2d(32))
-            l += 1
-                  
-
-        for i in range(n):
-            if i == 0:
-                self.model.add_module(f'conv{l}', nn.Conv2d(32, 64, 1, stride=2, bias=False)) # k_size = 1, stride = 2, pad = 0 for downsampling
-            else:
-                self.model.add_module(f'conv{l}', nn.Conv2d(64, 64, 3, stride=1, padding=1, bias=False)) # k_size = 3, stride = 1
-            self.model.add_module(f'bn{l}', nn.BatchNorm2d(64))
-            self.model.add_module(f'relu{l}', nn.ReLU())
-            self.relu_list[l] = 1
-            l += 1
-            self.model.add_module(f'conv{l}', nn.Conv2d(64, 64, 3, stride=1, padding=1, bias=False))
-            self.model.add_module(f'bn{l}', nn.BatchNorm2d(64))
-            l += 1
-            
         self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
 
         self.fc = nn.Sequential(
@@ -76,6 +43,25 @@ class CifarResNet(nn.Module):
             nn.LogSoftmax(dim=1)
         )
 
+    def grouped_resnet_blocks(self, in_c, out_c, sub=0):
+
+        for i in range(n):
+            if i == 0 and in_c != out_c:    # condition for downsample and subsample
+                self.model.add_module(f'conv{self.l}', nn.Conv2d(in_c, out_c, 1, stride=2, bias=False)) # downsampling
+            else:
+                self.model.add_module(f'conv{self.l}', nn.Conv2d(out_c, out_c, 3, stride=1, padding=1, bias=False))
+            self.model.add_module(f'bn{self.l}', nn.BatchNorm2d(out_c))
+            self.model.add_module(f'relu{self.l}', nn.ReLU())
+            self.relu_list[self.l] = 1
+            self.l += 1
+            self.model.add_module(f'conv{self.l}', nn.Conv2d(out_c, out_c, 3, stride=1, padding=1, bias=False))
+            self.model.add_module(f'bn{self.l}', nn.BatchNorm2d(out_c))
+            self.l += 1
+
+        if in_c != out_c:    # condition for subsample
+            self.model.add_module(f'subSample{sub}_conv', nn.Conv2d(in_c, out_c, 1, stride=2, bias=False))
+            self.model.add_module(f'subSample{sub}_bn', nn.BatchNorm2d(out_c))
+            self.model.add_module(f'subSample{sub}_relu', nn.ReLU())
 
     # noinspection PyPep8Naming
     def forward(self, X):
@@ -84,28 +70,36 @@ class CifarResNet(nn.Module):
         """
 
         layers = {name:module for name, module in self.model.named_modules()}
-        id_dict = {f'identity{i}': None for i in range(3*self.n)}    # network will have 3n skip connections
+        id_dict = {f'id{i}': None for i in range(3*self.n)}    # network will have 3n skip connections
 
         out = layers['conv1'](X)
         out = layers['bn1'](out)
         out = layers['relu1'](out)
 
-        k = 0 
+        s = 0  # skip connections
         for i in range(2, 6 * self.n + 2):
-            id_dict[f'identity{k}'] = out   # residual
+            if id_dict[f'id{s}'] is None: 
+                id_dict[f'id{s}'] = out   # residual
+                print('-'*40, '>')
             out = layers[f'conv{i}'](out)
             out = layers[f'bn{i}'](out)
-            if self.relu_list[i] == 1: 
-                out = layers[f'relu{i}'](out)
-            if self.relu_list[i] == 0:
-                out += id_dict[f'identity{k}'] # residual added
-                out = F.relu(out)
-                k += 1
             
-            # Uncomment to verify    
-            if i == 2*self.n + 2 or i == 4*self.n + 2: print('-'*10) # Basically print "-" after 2n+1, 4n + 1 (2n + 2n + 1)
-            print(out.shape, '\t channels', out.shape[1], '\t layer', i) 
+            print(out.shape, '\t\t |') 
 
+            if self.relu_list[i] == 1:  # normal ReLU 
+                out = layers[f'relu{i}'](out)
+            else:   # Skip Connection
+                if s%n == 0 and s>0:
+                    sub_i = int(s/self.n) # subSample number
+                    id_dict[f'id{s}'] = layers[f'subSample{sub_i}_conv'](id_dict[f'id{s}'])
+                    id_dict[f'id{s}'] = layers[f'subSample{sub_i}_bn'](id_dict[f'id{s}'])
+                    id_dict[f'id{s}'] = layers[f'subSample{sub_i}_relu'](id_dict[f'id{s}'])
+                
+                out += id_dict[f'id{s}'] # residual added
+                print('<', '-'*40)
+                out = F.relu(out)
+                s += 1
+            
         out = self.avgpool(out)
         out = torch.flatten(out, 1)
         out = self.fc(out)
@@ -113,6 +107,6 @@ class CifarResNet(nn.Module):
         return out
 
 if __name__ == "__main__":
-    n = 3 # try 5, 7, 9, 11
+    n = 5 # try 5, 7, 9, 11
     model = CifarResNet(n)
     print(model(torch.rand((1, 3, 32, 32))))
