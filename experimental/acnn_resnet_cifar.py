a8b7caa2d16b83ca691d4aa7a4bc18aa699cb980
Syzygianinfern0 <spsharan2000@gmail.com>
2019-10-05 01:49:49 +0530

    Train loop cleaned up

--- experimental/acnn_resnet_cifar.py
+++ experimental/acnn_resnet_cifar.py
@@ -1,4 +1,5 @@
 from torch import nn
+import torch.nn.functional as F
 
 from benchmarks.models.cifar_resnet_v2 import resnet
 from utilities.train_helpers import grouped_conv
@@ -6,18 +7,29 @@ from utilities.train_helpers import grouped_conv
 
 class ACNN(nn.Module):
 
-    def __init__(self, n1=18, n2=5, in_channels=3, no_classes=10):
+    def __init__(self, n1=18, n2=3, in_channels=3, no_classes=10):
         super(ACNN, self).__init__()
         self.features_net = resnet(n=n1, in_channels=in_channels)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.bn2 = nn.BatchNorm2d(64)
         self.filters_net = resnet(n=n2, in_channels=in_channels)
-
-        self.fc = nn.Linear(64, no_classes)
+        self.fc = nn.Sequential(
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Linear(16, no_classes),
+            nn.LogSoftmax()
+        )
 
     # noinspection PyPep8Naming
     def forward(self, X):
+        # out1 = F.relu(self.bn1(self.features_net(X)))
+        # out2 = F.relu(self.bn2(self.features_net(X)))
         out1 = self.features_net(X)
         out2 = self.features_net(X)
 
         out = grouped_conv(out1, out2)
 
-        return self.fc(out)
+        out = self.fc(out)
+        return out
--- experimental/acnn_train_cifar.py
+++ experimental/acnn_train_cifar.py
@@ -7,26 +7,25 @@ from os.path import abspath, dirname
 import numpy as np
 import torch
 import torch.optim as optim
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
+from torch import nn
 from torch.utils.tensorboard import SummaryWriter
-from torchvision.datasets import CIFAR10
 
 sys.path.append(dirname(dirname(dirname(abspath(__file__)))))
 
-from utilities.train_helpers import get_directories, train, test
-from utilities.cifar_utils import std_cifar10, mean_cifar10
+from utilities.train_helpers import get_directories, test, train
 from utilities.visuals import plot_logs, write_csv, write_to_readme
-from experimental.Cifar_AcnnResNet import CifarAcnnResNet
+from experimental.acnn_resnet_cifar import ACNN
+from benchmarks.models.cifar_resnet_v2 import BenchmarkResNet
 from utilities.data import load_data
 
 
 def parse_train_args():
     parser = argparse.ArgumentParser("Features Network --> ResNet, Filters network --> Drastic Convolution")
     parser.add_argument("--n1", default=18, help="ResNet depth for Features Network")
+    parser.add_argument("--n2", default=3, help="ResNet depth for Filters Network")
     parser.add_argument("--bs", default=128, help="Batch Size for training data")
     parser.add_argument("--dataset", default='CIFAR10', help="Dataset for Training")
-    parser.add_argument("--epochs", default=200, help="Number of epochs")
+    parser.add_argument("--epochs", default=1, help="Number of epochs")
     parser.add_argument("--lr", default=0.1, help="Initial Learning Rate")
     parser.add_argument("--logs", default=True, help="TensorBoard Logging")
     parser.add_argument("--log-dir", default='./results', help="Directory to save logs")
@@ -40,7 +39,10 @@ def parse_train_args():
 
 # noinspection PyShadowingNames
 def adjust_learning_rate(args, optimizer, epoch):
-    if args.lr_schedule == 0:
+    # TODO: https://pytorch.org/docs/stable/optim.html#torch.optim.lr_scheduler.ReduceLROnPlateau
+    if epoch <= 10:
+        lr = 0.01
+    elif args.lr_schedule == 0:
         lr = args.lr * ((0.2 ** int(epoch >= 60)) * (0.2 ** int(epoch >= 120))
                         * (0.2 ** int(epoch >= 160) * (0.2 ** int(epoch >= 220))))
     elif args.lr_schedule == 1:
@@ -59,42 +61,38 @@ def adjust_learning_rate(args, optimizer, epoch):
 if __name__ == '__main__':
     args = parse_train_args()
 
+    torch.manual_seed(args.seed)
+
+    # ----------------------Load Data---------------------- #
     if args.dataset == 'CIFAR10':
         print('To train and eval on cifar10 dataset......')
         num_classes = 10
-        data_loc = './data'
+        data_loc = r'E:\Datasets'
 
-        train_loader, test_loader = load_data(data_loc, args.bs, dataset=args.dataset)
+        train_loader, test_loader = load_data(data_loc, args.bs, dataset=args.dataset, download=False)
     else:
         raise NotImplementedError('Only Cifar10 dataset')
 
-    torch.manual_seed(args.seed)
-
-    # logger_dir = os.path.join(dirname(dirname(abspath(__file__))), args_dir['log_dir'])
-    logger_dir = args.log_dir
-    if not os.path.exists(logger_dir):
-        os.mkdir(logger_dir)
-
     # ----------------------Initialise model---------------------- #
+    # noinspection PyUnresolvedReferences
+    # model = ACNN(n1=args.n1,
+    #              n2=args.n2).cuda()
 
-    model = CifarAcnnResNet(n=args.n1).cuda()
+    model = BenchmarkResNet(18).cuda()
     optimizer = optim.SGD(model.parameters(), lr=args.lr,
                           momentum=args.momentum,
                           nesterov=args.nesterov,
                           weight_decay=args.weight_decay)
+    criterion = nn.CrossEntropyLoss()
 
-    # ---------------------Get Logging Directories---------------- #
-
+    # ----------------------Get Logging Directories---------------------- #
+    # logger_dir = os.path.join(dirname(dirname(abspath(__file__))), args_dir['log_dir'])
+    logger_dir = args.log_dir
+    if not os.path.exists(logger_dir):
+        os.mkdir(logger_dir)
     training_dir, tensorboard_dir, save_models_dir = get_directories(model, args.dataset, logger_dir)
 
-    # -----------------------Hyperparams----------------------------- #
-
-    device = "cuda:0"
-    best_acc = 0
-    start_epoch = 0
-
-    # --------------------Tensorboard writer--------------------------- #
-
+    # ----------------------Tensorboard writer---------------------- #
     if args.logs:
         writer = SummaryWriter(tensorboard_dir)
         train_logger = (np.array([]), np.array([]), np.array([]))
@@ -102,28 +100,33 @@ if __name__ == '__main__':
     else:
         writer, train_logger, test_logger = None, None, None
 
-    # ---------------------Start Training------------------------------- #
+    # ----------------------Start Training---------------------- #
+    device = "cuda:0"
+    best_acc = 0
+    start_epoch = 1
 
     tick = time.time()
-    for epoch in range(start_epoch, args.epochs):
-        lr = adjust_learning_rate(args, optimizer, epoch + 1)
+    for epoch in range(start_epoch, args.epochs + 1):
+        # noinspection PyTypeChecker
+        lr = adjust_learning_rate(args, optimizer, epoch)
         train_logger = train(model, device,  # Train Loop
                              train_loader,
-                             optimizer, epoch,
+                             optimizer, epoch=epoch, criterion=criterion,
                              log_interval=100, writer=writer, logger=train_logger)
 
         test_logger, acc = test(model, device,  # Evaluation Loop
                                 test_loader, epoch,
+                                criterion=criterion,
                                 writer=writer, logger=test_logger)
 
-        # -------------Save best models------------------ #
+        # ----------------------Save best models---------------------- #
         if acc > best_acc:
             best_acc = acc
             torch.save(model.state_dict(), os.path.join(save_models_dir, 'best_epoch' + '.pth.tar'))
             torch.save(model.state_dict(), os.path.join(save_models_dir, 'epoch_%s' % epoch + '.pth.tar'))
     run_time = time.time() - tick
 
-    # ---------------------Save Logs-------------------------------------- #
+    # ----------------------Save Logs---------------------- #
 
     if args.logs:  # Plot log to graphs
         plot_logs(train_logger, training_dir)
